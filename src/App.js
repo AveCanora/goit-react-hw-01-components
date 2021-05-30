b5bd0491d9f53769f92abee7cf732da4a4b41933
Natalya Taranenko <go.avecanora@gmail.com>
2021-05-30 15:32:56 +0300

    task-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Profile from "./components/Profile";
+import user from "./user.json";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Главный компонент-контейнер приложения</h1>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Profile = ({ avatar, name, tag, location, stats }) => {
+  return (
+    <div className="profile">
+      Компонент "Профиль социальной сети"
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className="avatar"
+          width="200"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
