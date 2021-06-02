128be8794a526d1bfe1ae275e657f463ec209137
Natalya Taranenko <go.avecanora@gmail.com>
2021-06-02 10:22:14 +0300

    normalize

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
       <Statistics title="Upload stats" stats={statisticalData} />;
       <Statistics stats={statisticalData} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
