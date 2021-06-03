840c5bdc6b7fb2351e7b3c99a12328d4daf5904f
Natalya Taranenko <go.avecanora@gmail.com>
2021-06-03 10:27:08 +0300

    update

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,12 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+function randomColor (
+  r =  Math.floor(Math.random() * 100),
+  g= Math.floor(Math.random() * 100),
+  b= Math.floor(Math.random() * 100),
+);
+const styles = {
+  option: {
+    display: "inline-block",
+    width: 40,
+    height: 40,
+  },
+};
 const Statistics = ({ stats, title }) => (
   <section className="statistics">
     <h1>Секция статистики</h1>
     <h2 className="title">{title}</h2>
     <ul className="stat-list">
       {stats.map((stat) => (
-        <li className="item" key={stat.id}>
+        <li
+          className="item"
+          key={stat.id}
+          style={{
+            ...styles.option,
+            backgroundColor:
+              "rgb(" +
+              randomColor.r +
+              "," +
+              randomColor.g +
+              "," +
+              randomColor.b,
+          }}
+        >
           <span className="label">{stat.label}</span>
           <span className="percentage">{stat.percentage}%</span>
         </li>
